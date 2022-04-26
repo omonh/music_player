194443d9171d1e878324a9ab9242765894beabcc
Magnai0123 <Bayarmagnai8520@gmail.com>
2022-04-26 11:27:29 +0800

    MunhodApp

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/home_screen/home_screen_manager.dart';
+import 'package:music_player/music_player_screen/music_player_screen.dart';
 import 'package:music_player/service_location.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   void initState() {
     super.initState();
     getIt<HomeScreenManager>().updateSongs();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ValueListenableBuilder<List<String>>(
-        valueListenable: getIt<HomeScreenManager>().songListNotifer,
-        builder: (context, songList, child) {
-          return ListView.builder(
-            itemCount: songList.length,
-            itemBuilder:(context, index){
-            return  Card(
-              child: ListTile(
-                title: Text(songList[index]),
-               ),
-             );
-            }, 
-           );
-        }
-      ),
-     );
+          valueListenable: getIt<HomeScreenManager>().songListNotifer,
+          builder: (context, songList, child) {
+            return ListView.builder(
+              itemCount: songList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(songList[index]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MusicPlayerScreen()),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          }),
+    );
   }
 }
---
+++ lib/music_player_screen/music_player_screen.dart
@@ -0,0 +1,85 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:music_player/music_player_screen/music_player_screen_manager.dart';
+import 'package:music_player/service_location.dart';
+
+
+class MusicPlayerScreen extends StatefulWidget {
+  const MusicPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final manager = getIt<MusicPlayerScreenManager>();
+
+  @override
+  void initState() {
+    super.initState();
+    manager.loadSong();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Music Player')),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const Image(
+            image: AssetImage('assets/music.jpg'),
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+          ColoredBox(
+            color: Colors.white.withOpacity(0.7),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Spacer(),
+                  ProgressBar(
+                    progress: Duration(minutes: 1),
+                    total: Duration(minutes: 3, seconds: 30),
+                  ),
+                  ValueListenableBuilder<PlayingState>(
+                    valueListenable: manager.playingStateNotifier,
+                    builder: (context, playingState, child) {
+                      switch (playingState) {
+                        case PlayingState.loading:
+                          return const CircularProgressIndicator();
+
+                        case PlayingState.playing:
+                          return IconButton(
+                            onPressed: () {
+                              manager.pauseSong();
+                            },
+                            icon: Icon(Icons.pause),
+                          );
+
+                        case PlayingState.paused:
+                          return IconButton(
+                            onPressed: () {
+                              manager.playSong();
+                            },
+                            icon: Icon(Icons.play_arrow),
+                          );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
